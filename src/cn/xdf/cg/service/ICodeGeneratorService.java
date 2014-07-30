ad523d337bd38a8d80e70799baf602df867e8340
conntsing <conntsing@gmail.com>
2014-07-30 10:28:13 +0800

    base line

---
+++ src/cn/xdf/cg/service/ICodeGeneratorService.java
@@ -0,0 +1,13 @@
+package cn.xdf.cg.service;
+
+/**
+ * 代码生成器业务接口
+ * 
+ * @author qiusen
+ * 
+ * @date 2010-01-20
+ *
+ */
+public interface ICodeGeneratorService {
+
+}
---
+++ src/cn/xdf/cg/service/impl/CodeGeneratorServiceImpl.java
@@ -0,0 +1,15 @@
+package cn.xdf.cg.service.impl;
+
+import cn.xdf.cg.service.ICodeGeneratorService;
+
+/**
+ * 代码生成器业务接口 ICodeGeneratorService 实现类
+ * 
+ * @author qiusen
+ * 
+ * @date 2010-01-20
+ *
+ */
+public class CodeGeneratorServiceImpl implements ICodeGeneratorService {
+
+}
